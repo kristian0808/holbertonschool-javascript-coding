8a0506291dc5b62c951d8dfd6efad3278b5cb9cf
kristian0808 <kristianlleshaj@hotmail.com>
2024-05-28 21:23:07 +0200

    update

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,33 +2,25 @@ const http = require('http');
 const url = require('url');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((request, response) => {
-    const path = url.parse(request.url).pathname;
-
+const app = http.createServer((require, result) => {
+    const path = url.parse(require.url).pathname;
     if (path === '/') {
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.end('Hello Holberton School!');
+        result.end('Hello Holberton School!');
     } else if (path === '/students') {
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.write('This is the list of our students\n');
-
+        result.write('This is the list of our students\n');
         countStudents(process.argv[2])
             .then((data) => {
-                response.write(`Number of students: ${data.students.length}\n`);
+                result.write(`Number of students: ${data.students.length}\n`);
                 for (const field in data.subjects) {
                     if (Object.prototype.hasOwnProperty.call(data.subjects, field)) {
-                        response.write(`Number of students in ${field}: ${data.subjects[field].length}. List: ${data.subjects[field].join(', ')}\n`);
+                        result.write(`Number of students in ${field}: ${data.subjects[field].length}. List: ${data.subjects[field].join(', ')}\n`);
                     }
                 }
-                response.end();
+                result.end();
             })
             .catch((error) => {
-                response.write('Cannot load the database\n');
-                response.end();
+                result.end(error.message);
             });
-    } else {
-        response.writeHead(404, { 'Content-Type': 'text/plain' });
-        response.end('Not Found');
     }
 });
 
